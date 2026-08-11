ebc2cfef89d1304c3c2dddfd00341396332180f2
ORGATHM <[email address]>
2026-08-11 21:59:58 +0200

    graphs-v24

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 #include <WiFiClientSecure.h>
 
 // Hardcoded Firmware Version (incremented on each release)
-const int localFirmwareVersion = 23;
+const int localFirmwareVersion = 24;
 
 // Sensor Libraries
 #include <Adafruit_BME280.h>
